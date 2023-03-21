edc1cd09d79a7dc65d087fa6e033fa4d982e1fe0
xenoblade3 <edgarmacfoy@gmail.com>
2023-03-20 19:57:43 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,7 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
+int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
